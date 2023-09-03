26a262d7dc4b7af8c8aaabcb6699c7a1c19ae6d0
Maksym Vdovenko <jackjackjack1376@gmail.com>
2023-09-03 14:16:14 +0900

    Landing of pieces

--- lib/board.dart
+++ lib/board.dart
@@ -1,10 +1,29 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:tetris_game/piece.dart';
 import 'package:tetris_game/values.dart';
 
 import 'pixel.dart';
+
+/*
+GAME BOARD
+This is 2x2 grid with null representative an empty space .
+A non empty space will have the color representative the landed pieces
+
+
+*/
+
+//game board
+
+List<List<Tetromino?>> gameBoard = List.generate(
+    colLenght,
+    (i) => List.generate(
+          rowLength,
+          (j) => null,
+        ));
 
 class GameBoard extends StatefulWidget {
   GameBoard({super.key});
@@ -29,7 +48,7 @@ class _GameBoardState extends State<GameBoard> {
     currentPiece.initializedPiece();
     //frame refresh rate\
 
-    Duration frameRate = const Duration(milliseconds: 800);
+    Duration frameRate = const Duration(milliseconds: 400);
     gameLoop(frameRate);
   }
 
@@ -38,19 +57,58 @@ class _GameBoardState extends State<GameBoard> {
   void gameLoop(Duration frameRate) {
     Timer.periodic(frameRate, (timer) {
       setState(() {
+        checkLanding();
+
         //move piece down'
         currentPiece.movePiece(Direction.down);
       });
     });
   }
-  //collision detection 
- bool checkCollision(Direction direction){
 
+  //collision detection
+  bool checkCollision(Direction direction) {
     for (int i = 0; i < currentPiece.position.length; i++) {
-      
-    }
- }
+      int row = (currentPiece.position[i] / rowLength).floor();
+      int col = currentPiece.position[i] % rowLength;
 
+      if (direction == Direction.left) {
+        col -= 1;
+      } else if (direction == Direction.right) {
+        col += 1;
+      } else if (direction == Direction.down) {
+        row += 1;
+      }
+
+      //check out of bounds
+
+      if (row >= colLenght || col < 0 || col >= rowLength) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void checkLanding() {
+    if (checkCollision(Direction.down)) {
+      for (int i = 0; i < currentPiece.position.length; i++) {
+        int row = (currentPiece.position[i] / rowLength).floor();
+        int col = currentPiece.position[i] % rowLength;
+        if (row >= 0 && col >= 0) {
+          gameBoard[row][col] = currentPiece.type;
+        }
+      }
+      createNewPiece();
+    }
+  }
+
+  void createNewPiece() {
+    Random rand = Random();
+
+    Tetromino randomType =
+        Tetromino.values[rand.nextInt(Tetromino.values.length)];
+    currentPiece = Piece(type: randomType);
+    currentPiece.initializedPiece();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,12 +120,22 @@ class _GameBoardState extends State<GameBoard> {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: rowLength),
         itemBuilder: (context, index) {
+          int row = (index / rowLength).floor();
+          int col = index % rowLength;
+
+          //current piece
           if (currentPiece.position.contains(index)) {
             return Pixel(
               color: Colors.yellow,
               child: index,
             );
-          } else {
+          }
+          //landed piece
+          else if (gameBoard[row][col] != null) {
+            return Pixel(color: Colors.pink, child: '');
+          }
+          //blank pixel
+          else {
             return Pixel(
               color: Colors.grey[900],
               child: index,
