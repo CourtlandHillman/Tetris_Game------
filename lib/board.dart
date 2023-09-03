d7993d18db6f9dc49b21ac8c0a747ebc6ac15190
Maksym Vdovenko <jackjackjack1376@gmail.com>
2023-09-03 13:36:28 +0900

    Collision detector

--- lib/board.dart
+++ lib/board.dart
@@ -1,32 +1,80 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:tetris_game/piece.dart';
+import 'package:tetris_game/values.dart';
+
+import 'pixel.dart';
 
 class GameBoard extends StatefulWidget {
-  const GameBoard({super.key});
+  GameBoard({super.key});
 
   @override
   State<GameBoard> createState() => _GameBoardState();
 }
 
 class _GameBoardState extends State<GameBoard> {
-  int rowLength = 10;
-  int colLenght = 15;
+  //current tetris piece
+  Piece currentPiece = Piece(type: Tetromino.T);
+
+  @override
+  void initState() {
+    super.initState();
+
+    //start game
+    startGame();
+  }
+
+  void startGame() {
+    currentPiece.initializedPiece();
+    //frame refresh rate\
+
+    Duration frameRate = const Duration(milliseconds: 800);
+    gameLoop(frameRate);
+  }
+
+  //gameloop
+
+  void gameLoop(Duration frameRate) {
+    Timer.periodic(frameRate, (timer) {
+      setState(() {
+        //move piece down'
+        currentPiece.movePiece(Direction.down);
+      });
+    });
+  }
+  //collision detection 
+ bool checkCollision(Direction direction){
+
+    for (int i = 0; i < currentPiece.position.length; i++) {
+      
+    }
+ }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body:GridView.builder(
-        itemCount: rowLength*colLenght,
+      body: GridView.builder(
+        itemCount: rowLength * colLenght,
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: rowLength), 
-          itemBuilder: (context , index) => Center(
-            child: Text(
-              index.toString(),
-              style: TextStyle(color: Colors.white),
-              ) ,
-            ),
-           ) ,
+            crossAxisCount: rowLength),
+        itemBuilder: (context, index) {
+          if (currentPiece.position.contains(index)) {
+            return Pixel(
+              color: Colors.yellow,
+              child: index,
+            );
+          } else {
+            return Pixel(
+              color: Colors.grey[900],
+              child: index,
+            );
+          }
+        },
+      ),
     );
   }
 }
---
+++ lib/values.dart
@@ -0,0 +1,19 @@
+  int rowLength = 10;
+  int colLenght = 15;
+
+enum Direction{
+  left , 
+  right,
+  down,
+}
+
+enum Tetromino {
+  L,
+  J,
+  I,
+  O,
+  S,
+  Z,
+  T,
+
+}
